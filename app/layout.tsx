5c25f65555a222029ddbea97f0fae4b0a04adb86
Yash Chitneni <yash.chitneni@gmail.com>
2024-10-24 14:19:58 -0500

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={inter.className}>
         <NavBar />
         <main className="flex-grow">{children}</main>
